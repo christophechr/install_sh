f98dfeffa4f83facdf0b61bb74da30526dfd29c6
Christophe CHHOR <91553820+christophechr@users.noreply.github.com>
2023-10-22 15:10:53 +0200

    Update package.sh : Remove unused packages

--- package.sh
+++ package.sh
@@ -50,29 +50,9 @@ if [ -f /etc/fedora-release ]; then
     dnf install -y ncurses-devel
 fi
 
-#install criterion
-rpm -ivh https://github.com/samber/criterion-rpm-package/releases/download/2.3.3/libcriterion-devel-2.3.3-2.el7.x86_64.rpm
-
-#install gradle
-wget https://services.gradle.org/distributions/gradle-7.0-bin.zip && mkdir /opt/gradle && unzip -d /opt/gradle gradle-7.0-bin.zip && rm -f gradle-7.0-bin.zip
-
-#install stack
-curl -sSL https://get.haskellstack.org/ | sh
-
 #install norma2
 pip install -U norma2
 sudo norma2 --install-completion
-
-#config emacs
-git clone https://github.com/Epitech/epitech-emacs.git
-cd epitech-emacs
-git checkout 278bb6a630e6474f99028a8ee1a5c763e943d9a3
-./INSTALL.sh system
-cd .. && rm -rf epitech-emacs
-
-#change alias emacs GUI to terminal emacs
-if ["$bash" == "bash"]; then
-    echo alias emacs="emacs -nw" >> ~/.bashrc
 elif ["$bash" == "zsh"]; then
     echo alias emacs="emacs -nw" >> ~/.zshrc
 fi
